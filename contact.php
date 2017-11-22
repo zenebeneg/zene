0db2c77b6cb2f59b70009e0c3a761261a6d61515
zenebe <zenebenegash6@gmail.com>
2017-11-22 13:09:31 -0800

    hospital site

---
+++ contact.php
@@ -0,0 +1,80 @@
+<html>
+<head>
+	<title> UK Hospital </title>
+	
+	<link href="css/style.css" type="text/css" rel="stylesheet">
+	
+</head>
+<body>
+	
+	<div class="top">
+		<div>
+		 Contact Us +91 96366 20000 / 01 | ukh@hospital.com 
+		</div>
+	</div>
+	
+	<div class="logo">
+		<div>
+			<table>
+				<tr>
+					<td width="600px" style="font-size:50px;font-family:forte;"> <font color="#428bca"> UK  </font><font color="#000"> Hospital</font> </td>
+					<td> <br> <br>
+						<font size="4px"> 
+							<a href="index.php">HOME</a> 
+							<a href="about.php">ABOUT US</a>  
+							<a href="service.php">SERVICE</a>
+							<a href="doctor.php">DOCTORS</a> 
+							<a href="contact.php">CONTACT US</a>
+						</font>
+					</td>
+				</tr>
+			</table>
+		</div>
+	</div>
+	
+	
+	
+	
+	
+	<div class="bottom">
+		<div>
+			<table border="0">
+				<tr>
+					<td width="700px">
+						<font color="#000">------------ </font> <br> <br>
+
+					<font color="#000" size="6px"> CONTACT US </font> <br> <br>
+
+					Uk Hospital & Research Center
+					
+					<br>
+					<br>
+					
+					Mob : +91 0123456789
+					
+					<br>
+					<br>
+					
+					Email : ukh@hospital.com
+
+<br>
+<br>
+<br>
+ </td>
+					
+					
+					<td style="padding-left:20px;"> </td>
+				</tr>
+			
+				
+			</table>
+		</div>
+	</div>
+	
+	
+	<div class="nav_down">
+		<div>
+		 &copy; UK Hospital, site designed & developed by THARE_VASTE
+		</div>
+	</div>
+	
